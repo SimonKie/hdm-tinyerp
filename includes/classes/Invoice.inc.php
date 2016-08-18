b2d006836f80b4598eac58b5ab8d48f1dd2e1f9d
dmiedtank <d.miedtank@miedtank.com>
2016-08-18 14:53:59 +0200

    Directory structure

---
+++ includes/classes/Invoice.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Daniel
+ * Date: 18.08.2016
+ * Time: 14:51
+ */
+class Invoice
+{
+
+}
---
+++ includes/mapper/DBConnection.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Daniel
+ * Date: 18.08.2016
+ * Time: 14:48
+ */
+class DBConnection
+{
+
+}
