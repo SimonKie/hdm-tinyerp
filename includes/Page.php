32892bd9bac3c86b82b80dd621fe7548a7138904
dmiedtank <d.miedtank@miedtank.com>
2016-08-22 23:59:18 +0200

    Mapper

---
+++ includes/Page.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Daniel
+ * Date: 22.08.2016
+ * Time: 23:46
+ */
+class Page
+{
+    private $Title = "";
+    private $Content = "";
+    private $RightCol = "";
+
+    private $MainNav = array(
+        'Home' => 'index.php',
+        'Rechnungen' => 'invoice.php',
+        'Kunden' => 'customers.php'
+    );
+
+    /**
+     * @param string $Title
+     */
+    public function setTitle($Title)
+    {
+        $this->Title = $Title;
+    }
+
+    /**
+     * @param string $Content
+     */
+    public function setContent($Content)
+    {
+        $this->Content = $Content;
+    }
+
+    /**
+     * @param string $rightCol
+     */
+    public function setRightCol($RightCol)
+    {
+        $this->RightCol = $RightCol;
+    }
+
+    /**
+     * @return string
+     */
+    private function getHeader()
+    {
+        return '';
+    }
+
+    /**
+     * @return string
+     */
+    private function getMainNav()
+    {
+        $return = '<ul>';
+
+        foreach($this->MainNav as $key => $value)
+        {
+            $return .= '<li></li>';
+        }
+
+        $return .= '</ul>';
+
+        return $return;
+    }
+
+    /**
+     * @return string
+     */
+    private function getFooter()
+    {
+        return '';
+    }
+
+    public function run()
+    {
+
+    }
+
+
+
+}
