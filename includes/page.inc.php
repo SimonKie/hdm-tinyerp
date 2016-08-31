1d05fd3e9b96e3ac98e8414732ae8838323fbc9a
SimonKie <simon.kienzler@gmx.de>
2016-08-31 14:55:35 +0200

    deleted HOME_URL from css file path

--- includes/page.inc.php
+++ includes/page.inc.php
@@ -78,7 +78,7 @@ class Page
         
         <!-- css -->
         <link rel=\"stylesheet\" href=\"http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\">
-        <link rel=\"stylesheet\" type=\"text/css\" href=\"" . HOME_URL . "/includes/css/main.css\" />
+        <link rel=\"stylesheet\" type=\"text/css\" href=\"includes/css/main.css\" />
         
         <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js\"></script>
         <script src=\"http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\"></script>
